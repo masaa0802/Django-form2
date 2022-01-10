ab65d682371c205e29cb578363e93d87402a5838
Your Name <masaakigoto0802@gmail.com>
2022-01-10 15:55:20 +0900

    ~160

--- FormApp/forms.py
+++ FormApp/forms.py
@@ -12,3 +12,14 @@ class StudentInsertForm(forms.ModelForm):
   class Meta:
     model = Students
     fields ='__all__'
+
+
+class StudentUpdateForm(forms.Form):
+
+  name = forms.CharField(label='名前')
+  age = forms.IntegerField(label='年齢')
+  grade = forms.IntegerField(label='学年')
+  picture = forms.FileField(label='ファイルアップロード', required=False)
+
+class StudentDeleteForm(forms.Form):
+  id = forms.IntegerField(widget=forms.HiddenInput)
